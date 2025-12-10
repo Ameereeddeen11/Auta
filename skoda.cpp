f15073357b2b83e3b397fd02c25a0116c1951ce0
jakubkovarik <jakkovar103@gmail.com>
2025-12-10 11:20:32 +0100

    druhy commit

---
+++ skoda.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Jakub Kovařík on 10.12.2025.
+//
+#include "skoda.h"
---
+++ skoda.h
@@ -0,0 +1,13 @@
+//
+// Created by Jakub Kovařík on 10.12.2025.
+//
+
+#ifndef AUTA_SKODA_H
+#define AUTA_SKODA_H
+
+
+class skoda {
+};
+
+
+#endif //AUTA_SKODA_H
